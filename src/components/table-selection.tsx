1d2dfb4b8345a45e433154e35411c940adcf5ed5
dangpg <dangpg@users.noreply.github.com>
2023-04-13 21:09:55 -0500

    Add navigation to new order page

--- src/components/table-selection.tsx
+++ src/components/table-selection.tsx
@@ -12,24 +12,33 @@ const TableSelection = ({
   onSubmitTable,
 }: Props) => {
   return (
-    <VStack alignItems="stretch" gap={10}>
+    <VStack
+      alignItems="stretch"
+      gap={10}
+      bg="burlywood"
+      height="100%"
+      justifyContent="space-between"
+    >
       <Grid
-        gap={100}
+        gap={300}
         templateRows="repeat(2, 1fr)"
         templateColumns="repeat(3, 1fr)"
+        padding={10}
       >
         {[1, 2, 3, 4, 5, 6].map((tableNo) => (
           <GridItem
             key={tableNo}
-            bg="cadetblue"
+            bg={tableNo === selectedTableNo ? "green.600" : "gray.600"}
             border={
-              tableNo === selectedTableNo ? "2px" : "2px solid transparent"
+              tableNo === selectedTableNo
+                ? "2px solid white"
+                : "2px solid transparent"
             }
             onClick={() => onClickTable(tableNo)}
             minHeight={200}
           >
             <Center height="100%">
-              <Text fontSize="2xl">{tableNo}</Text>
+              <Text color="white" fontSize="2xl">{`#${tableNo}`}</Text>
             </Center>
           </GridItem>
         ))}
@@ -37,10 +46,11 @@ const TableSelection = ({
       <Button
         isDisabled={selectedTableNo === undefined}
         onClick={onSubmitTable}
+        size="lg"
       >
         {selectedTableNo === undefined
           ? "Please select a table to continue"
-          : `Create order for table ${selectedTableNo}`}
+          : `Create order for table #${selectedTableNo}`}
       </Button>
     </VStack>
   );
--- src/pages/new.tsx
+++ src/pages/new.tsx
@@ -1,7 +1,9 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Flex, Heading, HStack, VStack, Icon } from "@chakra-ui/react";
 import { useState } from "react";
 import TableSelection from "../components/table-selection";
 import Order from "../components/order";
+import { Link } from "react-router-dom";
+import { ChevronLeftIcon } from "@chakra-ui/icons";
 
 type Step = "Select table" | "Create order";
 
@@ -22,17 +24,42 @@ const New = () => {
   };
 
   return (
-    <Box h="100vh" padding={5}>
-      {step === "Create order" && tableNo !== undefined ? (
-        <Order tableNo={tableNo} onCancelOrder={handleCancelOrder} />
-      ) : (
-        <TableSelection
-          selectedTableNo={tableNo}
-          onClickTable={handleClickTable}
-          onSubmitTable={handleSubmitTable}
-        />
-      )}
-    </Box>
+    <Flex h="100vh" direction="column" gap={0}>
+      <Flex bg="blue.300" padding={2}>
+        <HStack>
+          {step === "Select table" ? (
+            <Link to="/">
+              <ChevronLeftIcon boxSize={6} aria-label="Back to Home" />
+            </Link>
+          ) : (
+            <Icon
+              as={ChevronLeftIcon}
+              boxSize={6}
+              onClick={handleCancelOrder}
+              cursor="pointer"
+            />
+          )}
+          <Heading whiteSpace="nowrap" size="lg">
+            {`${
+              step === "Select table"
+                ? "Step 1 of 2: Select Table"
+                : "Step 2 of 2: Create Order"
+            }`}
+          </Heading>
+        </HStack>
+      </Flex>
+      <Box flex={1}>
+        {step === "Create order" && tableNo !== undefined ? (
+          <Order tableNo={tableNo} onCancelOrder={handleCancelOrder} />
+        ) : (
+          <TableSelection
+            selectedTableNo={tableNo}
+            onClickTable={handleClickTable}
+            onSubmitTable={handleSubmitTable}
+          />
+        )}
+      </Box>
+    </Flex>
   );
 };
 
